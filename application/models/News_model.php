f52fa39a983460ab6835fef04b692b6acd31979b
ssniranga <nirangamail@gmail.com>
2016-01-09 01:35:18 +0530

    news

---
+++ application/models/News_model.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Niranga
+ * Date: 1/9/2016
+ * Time: 12:52 AM
+ */
+class News_model extends CI_Model {
+
+    public function __construct()
+    {
+        $this->load->database();
+    }
+
+    public function get_news($slug = FALSE)
+    {
+        if ($slug === FALSE)
+        {
+            $query = $this->db->get('news');
+            return $query->result_array();
+        }
+
+        $query = $this->db->get_where('news', array('slug' => $slug));
+        return $query->row_array();
+    }
+}
---
+++ application/views/news/view.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Niranga
+ * Date: 1/9/2016
+ * Time: 1:00 AM
+ */
+
+echo '<h2>' . $news_item['title'] . '</h2>';
+echo $news_item['text'];
